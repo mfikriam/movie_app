8c576a6790d28997e878b9df90db02ceb05346c4
mfikriam <yapkun17@gmail.com>
2024-06-25 21:59:16 +0800

    Add search components

---
+++ src/components/search/CategorySearch.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const CategorySearch = (): JSX.Element => {
+  return <Text>Category</Text>
+}
+
+export default CategorySearch
---
+++ src/components/search/KeywordSearch.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const KeywordSearch = (): JSX.Element => {
+  return <Text>Keyword</Text>
+}
+
+export default KeywordSearch
